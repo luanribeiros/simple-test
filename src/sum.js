e961e5709233dcbd3e2c4cdcb19b2e6f530ddc3a
Luan Ribeiro <luanribeiros@gmail.com>
2018-01-24 00:11:12 -0200

    implementando novo teste

--- src/sum.js
+++ src/sum.js
@@ -4,6 +4,9 @@ var sum = function sum(num1, num2) {
   if(!num1 || !num2) {
     return new Error('Passe dois números por parâmetros');
   }
+  if(typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return new Error('Os parâmetros devem ser números');
+  }
   return num1 + num2;
 }
 
