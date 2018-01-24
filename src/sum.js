693a200ed8f9eb48384202afd83f46be72d2aa50
Luan Ribeiro <luanribeiros@gmail.com>
2018-01-24 00:07:56 -0200

    implementando novos testes a nosso exemplo

--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,10 @@
 'use strict';
 
-module.exports = function() {};
+var sum = function sum(num1, num2) {
+  if(!num1 || !num2) {
+    return new Error('Passe dois números por parâmetros');
+  }
+  return num1 + num2;
+}
+
+module.exports = sum;
